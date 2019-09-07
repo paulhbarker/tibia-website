694a2f989f210b320a4616c3862afaa78c0f2c30
paulhbarker <paul_barker@live.com>
2019-09-06 18:10:52 -0700

    add unversioned files

---
+++ app/Account.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Account extends Model
+{
+    protected $fillable = [
+        'id',
+        'name',
+        'password',
+        'secret',
+        'type',
+        'premdays',
+        'lastday',
+        'email',
+        'creation'
+    ];
+
+    public $timestamps = false;
+}
---
+++ database/migrations/2019_08_31_010546_initial.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Initial extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Eloquent::unguard();
+
+        $path = 'database/tables.sql';
+
+        DB::unprepared(file_get_contents($path));
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
